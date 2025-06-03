dbe586086dc82c1968c09452f0e4a11d4f815cff
Alex Mandel <tech@wildintellect.com>
2025-06-03 14:02:59 -0700

    fix: need to set a cran mirror

--- install.R
+++ install.R
@@ -1,6 +1,7 @@
+repo = "https://cloud.r-project.org/"
 package_list <- c("yaml","ncdf4","plyr","dplyr","parallel","abind","Matrix","lattice",
     "memuse","gplots","EnvStats","gridExtra","mvtnorm","plotly","MASS","svd","e1071",
     "png","lattice","stats","repr","ggplot2","txtplot","gplots","fields","data","tools",
     "plotrix","bits","spam","MixMatrix") 
 
-install.packages(package_list)
+install.packages(package_list, repos=repo)
