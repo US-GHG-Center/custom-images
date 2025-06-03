db0d0ef7644f4ca28f29ca76e173a8605f639e85
Alex Mandel <tech@wildintellect.com>
2025-06-03 13:56:35 -0700

    fix: Typo missing ,

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-package_list <- c("yaml","ncdf4","plyr","dplyr""parallel","abind","Matrix","lattice",
+package_list <- c("yaml","ncdf4","plyr","dplyr","parallel","abind","Matrix","lattice",
     "memuse","gplots","EnvStats","gridExtra","mvtnorm","plotly","MASS","svd","e1071",
     "png","lattice","stats","repr","ggplot2","txtplot","gplots","fields","data","tools",
     "plotrix","bits","spam","MixMatrix") 
