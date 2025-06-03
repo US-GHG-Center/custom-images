1991ec670ff45e1cebaf66cc7923be59f4a724c5
Alex Mandel <tech@wildintellect.com>
2025-06-03 13:29:21 -0700

    feat: py-rocket-geospatial with custom pkgs

---
+++ install.R
@@ -0,0 +1,6 @@
+package_list <- c("yaml","ncdf4","plyr","dplyr""parallel","abind","Matrix","lattice",
+    "memuse","gplots","EnvStats","gridExtra","mvtnorm","plotly","MASS","svd","e1071",
+    "png","lattice","stats","repr","ggplot2","txtplot","gplots","fields","data","tools",
+    "plotrix","bits","spam","MixMatrix") 
+
+install.packages(list.of.packages)
