17b088bc5892a9da6ac38483fbe5a4e7bd168e92
Alex I. Mandel <alex@developmentseed.org>
2025-06-19 09:30:10 -0700

    gdata not data is the package name
    Caught an error in the package name

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 repo = "https://cloud.r-project.org/"
 package_list <- c("yaml","ncdf4","plyr","dplyr","parallel","abind","Matrix","lattice",
     "memuse","gplots","EnvStats","gridExtra","mvtnorm","plotly","MASS","svd","e1071",
-    "png","lattice","stats","repr","ggplot2","txtplot","gplots","fields","data","tools",
+    "png","lattice","stats","repr","ggplot2","txtplot","gplots","fields","gdata","tools",
     "plotrix","bits","spam","MixMatrix") 
 
 install.packages(package_list, repos=repo)
